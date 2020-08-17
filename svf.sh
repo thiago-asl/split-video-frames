8e97a897d0baaf65530441838032df3692a4eda3
Thiago Lopes <thiago.asl@outlook.com>
2020-08-16 22:20:41 -0300

    Update svf.sh

--- svf.sh
+++ svf.sh
@@ -6,8 +6,10 @@ short_name="$(basename "${input_file}" | sed 's/\(.*\)\..*/\1/')"
 target_dir="$(pwd)"/target
 
 rm -rfv "${target_dir}" \
-	&& mkdir -pv "${target_dir}"
+	&& mkdir -pv "${target_dir}" \
+	&& ffmpeg -i "${input_file}" -t 00:05 "${target_dir}"/"${short_name}"_%05d.png
 
 echo $input_file
 echo $short_name
 echo $target_dir
+echo $(ls -l $target_dir | wc -l)
