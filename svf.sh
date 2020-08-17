07cb305ef5f515a78992baa61e41464527033eba
Thiago Lopes <thiago.asl@outlook.com>
2020-08-17 12:35:00 -0300

    Update svf.sh

--- svf.sh
+++ svf.sh
@@ -7,5 +7,5 @@ target_dir="$(pwd)"/target
 
 rm -rf "${target_dir}" \
 	&& mkdir -pv "${target_dir}" \
-	&& ffmpeg -loglevel quiet -i "${input_file}" "${target_dir}"/"${short_name}"_%05d.png \
+	&& ffmpeg -loglevel error -i "${input_file}" "${target_dir}"/"${short_name}"_%05d.png \
 	&& echo "Created $(ls -l "${target_dir}" | wc -l) frames."
