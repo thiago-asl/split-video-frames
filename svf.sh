47bda441ba3bc332d25a9fd82e5321a5aedb6b5e
Thiago Lopes <thiago.asl@outlook.com>
2020-08-19 04:07:33 -0300

    Update svf.sh

--- svf.sh
+++ svf.sh
@@ -3,7 +3,7 @@ cd ${0%/*}
 
 input_file="${1}"
 short_name="$(basename "${input_file}" | sed 's/\(.*\)\..*/\1/')"
-target_dir="$(pwd)"/target
+target_dir="$(pwd)"/"${short_name}"
 
 rm -rf "${target_dir}" \
 	&& mkdir -pv "${target_dir}" \
